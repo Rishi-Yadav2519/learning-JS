2974f5374618cd755edcd7cea249e257919ee6d3
Rishi-Yadav2519 <rsyadav2519@gmail.com>
2025-12-24 12:06:48 +0000

    Learnt about datatypes

---
+++ 01_Basics/Datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // It is like a command to tell modules to treat JS as newer version even we cannot need to define this as well because by default JS treat our code as newer version now.
+
+//alert("Hello") // We are using nodeJS not browser and it is browser only snytax
+
+// Data Types
+
+// Number => Number data type is written directly
+// BigInt => Number not stored inside number data type will go in BigInt by defining it
+// String => Sting are text. Declared inside "" (Double Code) or '' (Single Code)
+// Boolean => Either true or false
+// Null => Standalone value (Representation of no value)
+// Undefined => Value not assigned
+// Symbol => Used for defining something unique rather in React most probably
+
+
+// Object
+
+let a = "Rishi"
+
+console.log(typeof a); // We can check typeof any variable or the value directly
+console.log(typeof null); // Type of null is object
+console.log(typeof undefined); // Type of undefined is undefined
